154976fc04c1370b7de9313ed590767a4ee6fabf
Carlos Montemuino <cmontemuino@users.noreply.github.com>
2020-07-06 16:26:30 +0200

    Image with file-store as backend (#5)

---
+++ 1.9/buster/file-store/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Variable BACKEND_STORE could has been overridden. We need to check it does point to an existing directory.
+if [ ! -d $BACKEND_STORE ]; then
+  printf 'Path |%s| does not exist. Please check you have mounted it as a volumne.\n' "$BACKEND_STORE" >&2
+  exit 1
+fi
+
+exec "$@" \
+    --backend-store-uri "file://${BACKEND_STORE}" \
+    --default-artifact-root "file://${BACKEND_STORE}" \
+    --host "$NETWORK_INTERFACE" \
+    --port 5000
+    --expose-prometheus /metrics
