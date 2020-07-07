2152fb2c4a559c7df827b48a43a0027ad9c0c1da
Carlos Montemuiño <cmontemuino@users.noreply.github.com>
2020-07-07 14:20:21 +0200

    Default backend store missing back-slash

--- 1.9/buster/fs/docker-entrypoint.sh
+++ 1.9/buster/fs/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Variable BACKEND_STORE could has been overridden. We need to check it does point to an existing directory.
 if [ ! -d $BACKEND_STORE ]; then
-  printf 'Path |%s| does not exist. Please check you have mounted it as a volumne.\n' "$BACKEND_STORE" >&2
+  printf 'Path |%s| does not exist. Please check you have mounted it as a volume.\n' "$BACKEND_STORE" >&2
   exit 1
 fi
 
